410db11a11252677226120cb4dffa1979abd463c
Calvin Yuen <scyuenab@gmail.com>
2023-01-08 22:01:46 +0800

    updates

--- ios/MyReactNativeTemplate/AppDelegate.mm
+++ ios/MyReactNativeTemplate/AppDelegate.mm
@@ -44,7 +44,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"testapp", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"MyReactNativeTemplate", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
--- ios/MyReactNativeTemplateTests/MyReactNativeTemplateTests.m
+++ ios/MyReactNativeTemplateTests/MyReactNativeTemplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface testappTests : XCTestCase
+@interface MyReactNativeTemplateTests : XCTestCase
 
 @end
 
-@implementation testappTests
+@implementation MyReactNativeTemplateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
